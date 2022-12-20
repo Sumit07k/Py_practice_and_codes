8bf3ae16b907b5c5115d2dd512d811eed41ff75d
Sumit Kumar <sumitkumarsk2001@gmail.com>
2022-12-20 23:44:25 +0530

    python program to copy of clone a list by various ways

---
+++ Lists/cloning_copying_lst.py
@@ -0,0 +1,24 @@
+l1 = [1,2,3,4,5,6]
+
+# method 1 (slicing)
+# l2 = l1[:]
+
+# print(l2)
+
+# method 2
+# l3 = []
+# l3.extend(l1)
+
+# print(l3)
+
+# method 3 (copying)
+# l4 = []
+# l4 = l1
+
+# method 4
+# l5 = []
+# for i in l1:
+#   l5.append(i)
+
+# print("list: ",l5) 
+
