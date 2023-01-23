99a16e19850c6b5a3d4aac2ab4e336992e70dcdb
Sumit Kumar <sumitkumarsk2001@gmail.com>
2023-01-23 23:18:28 +0530

    program to know the runner up score of a team

---
+++ Hackerrank/runner_up_score.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = map(int, input().split())
+
+# l1 = list(arr)
+# l1.sort()
+# maxx = l1[-1]
+# x = len(l1)-1
+   
+# while x!=-1:
+#   if l1[x] < maxx:
+#     maxx = l1[x]
+#     break
+#   x = x - 1
+# print(maxx)
+
+#  or 
+# s = set(arr)
+# sorted(s)
+# l = list(s)
+# print(l[-2])
